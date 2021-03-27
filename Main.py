608d8ad219295208bc2e3f069f2fc51ac040d8fa
badaszewskib <badaszewskib@gmail.com>
2021-03-27 10:55:19 -0400

    Broke up functionality to put it in a function

--- Main.py
+++ Main.py
@@ -1,21 +1,26 @@
 import cv2
 import os
 
-#Usage - place data to be used inside the data>input folder, output will
-#be placed into the data>output folder.
+# Usage - place data to be used inside the data>input folder, output will
+# be placed into the data>output folder.
 
-#Program execution begins here
+
+def feature_extract(image):
+    cv2.imshow("Image", img)
+    cv2.waitKey(1000)
+    cv2.destroyAllWindows()
+
+
+# Program execution begins here
 inputPath = 'data\input'
 
 for fileDir in os.listdir(inputPath):
     fileDir = inputPath + '\\' + fileDir
     try:
         if fileDir.endswith(".BMP"):
-            #CURRENT FUNCTIONALITY - OPEN INPUT IMAGES IN SUCCESSION
+            # CURRENT FUNCTIONALITY - OPEN INPUT IMAGES IN SUCCESSION
             img = cv2.imread(fileDir)
-            cv2.imshow("Image", img)
-            cv2.waitKey(1000)
-            cv2.destroyAllWindows()
+            feature_extract(img)
     except Exception as e:
         raise e
         print("File error")
