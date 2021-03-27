a67d22f8039c9d7e8754c0eaf25957e81f26da29
badaszewskib <badaszewskib@gmail.com>
2021-03-27 10:47:49 -0400

    Added file reading functionality

---
+++ Main.py
@@ -0,0 +1,21 @@
+import cv2
+import os
+
+#Usage - place data to be used inside the data>input folder, output will
+#be placed into the data>output folder.
+
+#Program execution begins here
+inputPath = 'data\input'
+
+for fileDir in os.listdir(inputPath):
+    fileDir = inputPath + '\\' + fileDir
+    try:
+        if fileDir.endswith(".BMP"):
+            #CURRENT FUNCTIONALITY - OPEN INPUT IMAGES IN SUCCESSION
+            img = cv2.imread(fileDir)
+            cv2.imshow("Image", img)
+            cv2.waitKey(1000)
+            cv2.destroyAllWindows()
+    except Exception as e:
+        raise e
+        print("File error")
