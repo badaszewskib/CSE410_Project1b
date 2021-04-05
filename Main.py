8705ef46939b8d6c2d94f43973997e272351f8c6
badaszewskib <badaszewskib@gmail.com>
2021-04-05 19:22:00 -0400

    Created image masking

--- Main.py
+++ Main.py
@@ -2,13 +2,35 @@ import cv2
 import os, math
 import numpy as np
 
-#  up image into 3px*3px chunks, makes and returns orientation map
+blockSize = 6  # About the size of one ridge + valley
+
+#  Get rid of whitespace background from fingerprint images
+def mask_bkgrnd(orig):
+    retVal = orig.copy()
+    h, w = orig.shape
+    mask = np.ones((h, w))
+    mask = mask.astype(np.uint8)
+    for y in range(0, h, blockSize):
+        for x in range(0, w, blockSize):
+            avgVal = 0
+            for a in range(y, y+blockSize):
+                for b in range(x, x+blockSize):
+                    avgVal += orig[a][b]
+            avgVal /= blockSize**2
+            if(avgVal > 254.0):
+                for i in range(y, y+blockSize):
+                    for j in range(x, x+blockSize):
+                        mask[i][j] = 0
+    retVal *= mask
+    return retVal
+
+
+#  up image into 6px*6px chunks, makes and returns orientation map
 def orientation_map(img):
-    blockSize = 3
     row = -1
     col = -1
     oMap = np.zeros((int(img.shape[1]/blockSize), int(img.shape[0]/blockSize)))  # Should be 32Y x 30X
-    numCols, numRows = oMap.shape
+    numCols, numRows = img.shape
     oMap = oMap.astype(np.double)
     varX = oMap.copy()
     varY = oMap.copy()
@@ -18,17 +40,18 @@ def orientation_map(img):
             col = col+1  # Increment col
             xPix = x*blockSize;
             yPix = y*blockSize;
-            xGrad = cv2.Sobel(img[xPix:xPix+blockSize, yPix:yPix+blockSize], -1, 1, 0)
-            yGrad = cv2.Sobel(img[xPix:xPix+blockSize, yPix:yPix+blockSize], -1, 0, 1)
-            for i in range(0, blockSize):
-                for j in range(0, blockSize):
-                    print(xGrad[i][j])
-                    varX[row][col] += ((xGrad[i][j])*(yGrad[i][j]))**2
-                    varY[row][col] += 2*((xGrad[i][j])*(yGrad[i][j]))
+            # print(sum(sum(img[yPix:yPix+blockSize][xPix:xPix+blockSize])))
+            if(sum(sum(img[yPix:yPix+blockSize][xPix:xPix+blockSize]) != 0)):
+                xGrad = cv2.Sobel(img[yPix:yPix+blockSize, xPix:xPix+blockSize], -1, 1, 0)
+                yGrad = cv2.Sobel(img[yPix:yPix+blockSize, xPix:xPix+blockSize], -1, 0, 1)
+                for i in range(0, blockSize):
+                    for j in range(0, blockSize):
+                        varX[row][col] += ((xGrad[i][j])*(yGrad[i][j]))**2
+                        varY[row][col] += 2*((xGrad[i][j])*(yGrad[i][j]))
         oMap[row][col] = 0.5*(math.atan(varY[row][col]/varX[row][col]))
-    col = -1
-    print(oMap)
+        col = -1
     cv2.blur(img, (2,2))
+    return oMap
 
 # SRC: https://medium.com/@cuevas1208/fingerprint-algorithm-recognition-fd2ac0c6f5fc
 # SRC: https://www.rroij.com/open-access/fingerprint-image-enhancementalgorithm-and-performance-evaluation.pdf
@@ -40,6 +63,7 @@ def feature_extract(img):
     img = img[3:99, 2:92]
     # Resulting image size: 96Y x 90X
 
+    img = mask_bkgrnd(img)
     orientation_map(img)
     # Display a blown up image x5
     newWidth = int(img.shape[1] * 5);
@@ -48,7 +72,7 @@ def feature_extract(img):
     rect = cv2.resize(img, dim)
     cv2.imshow("Image", rect)
     cv2.waitKey(0)
-    cv2.destroyAllWindows()
+    # cv2.destroyAllWindows()
 
 
 # !!! Program execution begins here !!!
@@ -58,7 +82,7 @@ for fileDir in os.listdir(inputPath):  # Loop thru all files in data/input direc
     fileDir = inputPath + '\\' + fileDir
     try:
         if fileDir.endswith(".BMP"):
-            image = cv2.imread(fileDir)  # Read the image file
+            image = cv2.imread(fileDir, cv2.IMREAD_GRAYSCALE)  # Read the image file
             feature_extract(image)  # Send to actual project function
     except Exception as e:
         raise e
